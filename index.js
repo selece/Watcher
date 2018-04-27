a9875b7496bfb95dd853e062f79ad2fe38b34789
selece <jonathan.liaw@gmail.com>
2018-04-27 03:28:39 -0600

    Changed request-es6 -> request-promise for better support/dev network

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const request = require('es6-request');
+const req = require('request-promise');
 const _ = require('lodash');
 const config = require('./config.js');
 
-request.get('https://www.alphavantage.co/query')
-  .query({
+const options = {
+  uri: 'https://www.alphavantage.co/query',
+  qs: {
     'function': 'TIME_SERIES_INTRADAY',
     'outputsize': 'compact',
     'datatype': 'json',
     'apikey': config.apikeys.alphavantage,
     'symbol': 'TSX:T',
     'interval': '60min'
+  },
+  headers: {
+    'User-Agent': 'Watcher'
+  },
+  json: true
+};
+
+req(options)
+  .then(resp => {
+    console.log('request good!', resp);
   })
-  .then(([body, res]) => {
-    console.log(body);
+  .catch(err => {
+    console.log('request failed!', err);
   });
